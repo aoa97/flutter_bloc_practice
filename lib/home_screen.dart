2d7e30815d0cd4928cc5c64a57e4317bf6c22b6d
Ahmed Usama <eng.ahmedusama@yahoo.com>
2025-05-12 10:50:14 +0300

    Reset master "Initialize empty project"

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,41 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_practice/cubit/counter_cubit.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final cubit = context.read<CounterCubit>();
-
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 20,
-          children: [
-            Builder(
-              builder: (_) => Text(
-                context.watch<CounterCubit>().state.value.toString(),
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-            ),
-            Wrap(
-              spacing: 10,
-              children: [
-                FilledButton.tonal(
-                  onPressed: cubit.decrement,
-                  child: const Text("Decrement"),
-                ),
-                FilledButton(
-                  onPressed: cubit.increment,
-                  child: const Text("Increment"),
-                ),
-              ],
-            ),
-          ],
-        ),
+        child: Text('HomeScreen'),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_practice/cubit/counter_cubit.dart';
 import 'package:flutter_bloc_practice/home_screen.dart';
 
 void main() {
@@ -13,10 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider<CounterCubit>(
-        create: (_) => CounterCubit(),
-        child: HomeScreen(),
-      ),
+      home: HomeScreen(),
     );
   }
 }
